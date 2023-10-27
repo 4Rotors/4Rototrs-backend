6e76442ad1d2f30522470d350ba0009d12e5b122
Mishka <Fiz-mat228@yandex.ru>
2023-10-27 11:28:21 +0300

    added start application

---
+++ src/main/java/com/example/dronecourier/DroneCourierApplication.java
@@ -0,0 +1,13 @@
+package com.example.dronecourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneCourierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneCourierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dronecourier/DroneCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dronecourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneCourierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
