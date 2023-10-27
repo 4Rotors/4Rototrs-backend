77f13a04ed0c118e3c64660ee035edf3a0083f8e
dany0k <zmaev.vsu@gmail.com>
2023-10-28 02:38:22 +0300

    test: add test date to check location on mobile app

---
+++ src/main/java/com/example/dronecourier/entity/dto/TrackNumberDto.java
@@ -0,0 +1,11 @@
+package com.example.dronecourier.entity.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrackNumberDto {
+    private String trackNumber;
+}
